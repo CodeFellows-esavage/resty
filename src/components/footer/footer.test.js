81e90733a3de52701b6080ce8129369c76daf34d
erik savage <erik.k.savage@gmail.com>
2022-02-15 16:58:09 -0800

    outlines test shell for Footer

---
+++ src/components/footer/footer.test.js
@@ -0,0 +1,9 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import Footer from './index';
+
+xdescribe('Testing the Footer component', () => {
+  it('should', () => {
+    //no testing requirements right now
+  });
+});
+
