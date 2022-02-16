b538fceb5bafeb59dda10db5bee8834e8770c0dd
erik savage <erik.k.savage@gmail.com>
2022-02-15 16:49:37 -0800

    adds conditional rendering for loading notification

--- src/components/results/index.jsx
+++ src/components/results/index.jsx
@@ -4,7 +4,10 @@ import './results.scss';
 function Results(props) {
   return (
     <section>
-      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      {
+        props.loading ? 'Loading...' :
+        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      }
     </section>
   );
 }
