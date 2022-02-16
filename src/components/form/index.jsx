e3e9dd38f1d4268b6215117c347f18e5c23c25bf
erik savage <erik.k.savage@gmail.com>
2022-02-15 16:48:45 -0800

    adds handleSelect function json text field

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,14 +1,38 @@
+/* eslint-disable default-case */
 import React from 'react';
+import { useState } from 'react';
 import './form.scss';
 
 function Form(props) {
 
+ let [method, updateMethod] = useState('GET');
+
+  function handleSelect(e){
+    switch (e.target.id){
+      case 'get':
+        updateMethod('GET');
+        break;
+      case 'post':
+        updateMethod('POST');
+        break;
+      case 'put':
+        updateMethod('PUT');
+        break;
+      case 'delete':
+        updateMethod('DELETE');
+        break;
+    };
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
     const formData = {
-      method: 'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: method,
+      url: e.target[0].value,
     };
+
+    if (method === 'POST' || method === "PUT") formData.jsonObj = e.target[2].value;
+    console.log(formData);
     props.handleApiCall(formData);
   }
 
@@ -18,14 +42,20 @@ function Form(props) {
           <label >
             <span>URL: </span>
             <input name='url' type='text' />
-            <button type="submit">GO!</button>
+            <button type="submit">SUBMIT</button>
           </label>
           <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
+            <span onClick={handleSelect} id="get">GET</span>
+            <span onClick={handleSelect} id="post">POST</span>
+            <span onClick={handleSelect} id="put">PUT</span>
+            <span onClick={handleSelect} id="delete">DELETE</span>
+          </label> 
+            {((method === 'POST' || method === 'PUT') && 
+              <label>JSON Object:
+                <input type="text"></input>
+              </label>
+            )}
+
         </form>
       </>
   );
